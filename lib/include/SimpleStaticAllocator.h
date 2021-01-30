374e28f51854d5d2a4b98dbc015f3aad450ebf7d
Ezhik452 <ezhik452@gmail.com>
2021-01-30 20:34:45 +0300

    + Забытые блокировки и описание.

--- lib/include/SimpleStaticAllocator.h
+++ lib/include/SimpleStaticAllocator.h
@@ -21,8 +21,15 @@ namespace AllocatorLib {
         ElementType pool[POOL_ELEMENTS_COUNT];
         SimpleStack<ElementTypePointer, POOL_ELEMENTS_COUNT> freeStack;
 
+        MutexLockFuntionType lock = nullptr;
+        MutexReleaseFuntionType release = nullptr;
+
     public:
         SimpleStaticAllocator(MutexLockFuntionType lockFunction, MutexReleaseFuntionType releaseFunction) {
+
+            lock = lockFunction;
+            release = releaseFunction;
+
             freeStack.init();
 
             for (uint32_t i = 0; i < POOL_ELEMENTS_COUNT; i++) {
@@ -35,22 +42,50 @@ namespace AllocatorLib {
         }
 
         size_t count() const noexcept {
-            return POOL_ELEMENTS_COUNT - freeStack.count();
+            if (lock != nullptr) {
+                lock();
+            }
+
+            size_t res = POOL_ELEMENTS_COUNT - freeStack.count();
+
+            if (release != nullptr) {
+                release();
+            }
+
+            return res;
         }
 
 
         ElementTypePointer allocate() {
-            if (!freeStack.isEmpty()) {
-                return freeStack.pop();
+            if (lock != nullptr) {
+                lock();
             }
 
-            return nullptr;
+            ElementTypePointer res = nullptr;
+
+            if (!freeStack.isEmpty()) {
+                res = freeStack.pop();
+            }
+
+            if (release != nullptr) {
+                release();
+            }
+
+            return res;
         }
 
         void free(ElementTypePointer &element) {
+            if (lock != nullptr) {
+                lock();
+            }
+
             if (!freeStack.isFull() && element != nullptr) {
                 freeStack.push(element);
                 element = nullptr;
+            }
+
+            if (release != nullptr) {
+                release();
             }
         }
     };
